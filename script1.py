1ff111b41718d6fb8f8fe59c6bed6371764be90a
mikhou <buisnessamirdabou@gmail.com>
2025-04-28 21:17:28 +0200

    new python script

---
+++ script1.py
@@ -0,0 +1,9 @@
+# Simple Python script: Greeting user
+
+def greet_user():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}! Welcome to the world of Python.")
+
+if __name__ == "__main__":
+    greet_user()
+
